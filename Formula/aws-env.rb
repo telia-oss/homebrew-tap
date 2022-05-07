3f36adeec8051b986071ad12e0b9659a18fc4693
goreleaserbot <goreleaser@carlosbecker.com>
2022-05-07 19:29:49 +0000

    Brew formula update for aws-env version v1.2.0

--- Formula/aws-env.rb
+++ Formula/aws-env.rb
@@ -5,19 +5,36 @@
 class AwsEnv < Formula
   desc "A small binary for securely handling secrets in environment variables on AWS."
   homepage "https://github.com/telia-oss/aws-env"
-  version "1.1.0"
+  version "1.2.0"
 
-  if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/telia-oss/aws-env/releases/download/v1.1.0/aws-env-1.1.0-darwin-amd64.tar.gz"
-    sha256 "14aef0fb72a9bdad8be8c5f37081c78943f9ffa8b1e76ffbb2491baa24bdec33"
-  end
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/telia-oss/aws-env/releases/download/v1.1.0/aws-env-1.1.0-linux-amd64.tar.gz"
-    sha256 "c4147fb38fdbfb276b398668591269de6f6a343deec699f8991eb9b52d850ef6"
+  on_macos do
+    url "https://github.com/telia-oss/aws-env/releases/download/v1.2.0/aws-env-1.2.0-darwin-amd64.tar.gz"
+    sha256 "9c06cb77bdbdbe6bca421245f790e43f1fe236a99368e63e3808aeacf4d1335a"
+
+    def install
+      bin.install "aws-env"
+    end
+
+    if Hardware::CPU.arm?
+      def caveats
+        <<~EOS
+          The darwin_arm64 architecture is not supported for the AwsEnv
+          formula at this time. The darwin_amd64 binary may work in compatibility
+          mode, but it might not be fully supported.
+        EOS
+      end
+    end
   end
 
-  def install
-    bin.install "aws-env"
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/telia-oss/aws-env/releases/download/v1.2.0/aws-env-1.2.0-linux-amd64.tar.gz"
+      sha256 "ee0c587dea289080b50b17efab1447e05e100d522357d9930083ba364769c4bb"
+
+      def install
+        bin.install "aws-env"
+      end
+    end
   end
 
   test do
